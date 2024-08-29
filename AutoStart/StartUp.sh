e06bddd49813453ebdf4c5bb0264fa5b1fcf388d
txqqltill <114397123+txqqltill@users.noreply.github.com>
2024-08-29 13:55:22 +0200

    Update StartUp.sh

--- AutoStart/StartUp.sh
+++ AutoStart/StartUp.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source /home/pi/Desktop/main_env/bin/activate
-python /home/pi/MagicMirror/modules/MMM-Face-Recognition-SMAI/MMM-Face-Recognition-SMAI.py | tee -a /home/pi/MagicMirror/MMM-Recog-output.log
+python /home/pi/MagicMirror/modules/MMM-Face-Recognition-SMAI/MMM-Face-Recognition-SMAI.py | tee -a /home/pi/MagicMirror/MMM-Face-Recognition-output.log
