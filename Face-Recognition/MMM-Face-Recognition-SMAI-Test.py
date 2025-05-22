3616eecab8b4602cbec2f04a286d416bb61186d2
Till Kleditzsch <tillkleditzsch@gmx.de>
2025-05-22 19:05:14 +0200

    fix detection

--- Face-Recognition/MMM-Face-Recognition-SMAI-Test.py
+++ Face-Recognition/MMM-Face-Recognition-SMAI-Test.py
@@ -44,9 +44,10 @@ while True:
 
     # Resize and convert to RGB
     small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
-    rgb_small_frame = small_frame[:, :, ::-1]
 
     # Detect faces
+    rgb_small_frame = np.ascontiguousarray(small_frame[:,:,::-1])
+
     face_locations = face_recognition.face_locations(rgb_small_frame)
     face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
 
