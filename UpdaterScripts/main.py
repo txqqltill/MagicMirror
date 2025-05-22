692e35a3ec2c13261080d3b67a511874c5b9a035
Till Kleditzsch <tillkleditzsch@gmx.de>
2025-05-22 20:18:17 +0200

    fix path

--- UpdaterScripts/main.py
+++ UpdaterScripts/main.py
@@ -1,5 +1,5 @@
 from core.app import App
 
 if __name__ == "__main__":
-    app = App(image_path="../modules/MMM-Face-Recognition-SMAI/public/")
+    app = App(image_path="/home/pi/MagicMirror/modules/MMM-Face-Recognition-SMAI/public/")
     app.run()
