e67625cc05f7f6dba669e7e353415950c4768455
Till Kleditzsch <tillkleditzsch@gmx.de>
2025-05-25 12:46:07 +0200

    Update MMM_Face_Recognition_SMAI_Test.py

--- Face-Recognition/MMM_Face_Recognition_SMAI_Test.py
+++ Face-Recognition/MMM_Face_Recognition_SMAI_Test.py
@@ -5,6 +5,7 @@ from MMM_Face_Recognition_SMAI_Common import (
     recognize_faces,
     log_person
 )
+from time import sleep
 import cv2
 
 video_capture = get_video()
@@ -13,12 +14,12 @@ known_face_encodings, known_face_names = load_known_faces()
 recognized_last = None
 
 print("🎥 Starting face recognition. Press 'q' to quit.")
+faces = []
 
 while True:
+    for _ in range(5):
+        ret, frame = video_capture.read()
     ret, frame = video_capture.read()
-    if not ret:
-        print("❌ Failed to read from camera.")
-        break
 
     face_encodings, face_locations = get_face_data(frame)
     names = recognize_faces(face_encodings, known_face_encodings, known_face_names)
@@ -33,9 +34,26 @@ while True:
         cv2.rectangle(frame, (left, bottom - 20), (right, bottom), (0, 0, 255), cv2.FILLED)
         cv2.putText(frame, name, (left + 4, bottom - 6), cv2.FONT_HERSHEY_DUPLEX, 0.5, (255, 255, 255), 1)
 
-    if names and names != recognized_last:
-        log_person("".join(names))
-        recognized_last = names
+    face_id = names[0] if names else "Guest"
+    faces.append(face_id)
+    if len(faces) > 4:
+        faces.pop(0)
+    if all(f == faces[0] for f in faces):
+        switch = True
+    else:
+        switch = False
+
+    sleep(0.5)
+
+    sample_txt = "/home/pi/MagicMirror/modules/MMM-Face-Recognition-SMAI/sample.txt"
+    with open(sample_txt) as f:
+        current_user = f.readline().strip('\n')
+    log_person(current_user)
+
+    if switch:
+        with open(sample_txt, "w") as f:
+            f.write(face_id)
+
 
     cv2.imshow("Face Recognition Test", frame)
 
