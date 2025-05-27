d8128fcfedfa82686ccd7341530c1bd316ad283e
txqqltill <114397123+txqqltill@users.noreply.github.com>
2025-05-27 11:31:25 +0200

    Fix face rec (#3)
    * Update MMM_Face_Recognition_SMAI_Test.py
    
    * extract commons

--- Face-Recognition/MMM_Face_Recognition_SMAI_Test.py
+++ Face-Recognition/MMM_Face_Recognition_SMAI_Test.py
@@ -3,8 +3,9 @@ from MMM_Face_Recognition_SMAI_Common import (
     get_video,
     get_face_data,
     recognize_faces,
-    log_person
+    send_data_to_MM
 )
+from time import sleep
 import cv2
 
 video_capture = get_video()
@@ -13,12 +14,12 @@ known_face_encodings, known_face_names = load_known_faces()
 recognized_last = None
 
 print("🎥 Starting face recognition. Press 'q' to quit.")
+faces = []
 
 while True:
+    for _ in range(5):
+        ret, frame = video_capture.read()
     ret, frame = video_capture.read()
-    if not ret:
-        print("❌ Failed to read from camera.")
-        break
 
     face_encodings, face_locations = get_face_data(frame)
     names = recognize_faces(face_encodings, known_face_encodings, known_face_names)
@@ -33,9 +34,7 @@ while True:
         cv2.rectangle(frame, (left, bottom - 20), (right, bottom), (0, 0, 255), cv2.FILLED)
         cv2.putText(frame, name, (left + 4, bottom - 6), cv2.FONT_HERSHEY_DUPLEX, 0.5, (255, 255, 255), 1)
 
-    if names and names != recognized_last:
-        log_person("".join(names))
-        recognized_last = names
+    send_data_to_MM(names, faces)
 
     cv2.imshow("Face Recognition Test", frame)
 
